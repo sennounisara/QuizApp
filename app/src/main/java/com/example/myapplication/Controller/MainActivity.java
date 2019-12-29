dfba5e0d1b24081390657004205a90ff5081ead4
sennounisara <sara.sennouni1996@gmail.com>
2019-12-29 09:27:38 +0100

    commit 12/29

--- app/src/main/java/com/example/myapplication/Controller/MainActivity.java
+++ app/src/main/java/com/example/myapplication/Controller/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.myapplication.Controller;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -20,14 +22,27 @@ public class MainActivity extends AppCompatActivity {
     private EditText mNameInput;
     private Button playButton;
     private User user;
+
+    public static final String PREF_KEY_USERNAME = "PREF_KEY_USERNAME";
+    public static final String PREF_KEY_SCORE = "PREF_KEY_SCORE";
+
+    private SharedPreferences sharedPreferences;
+
+    // ----- Identify for the gameActivity ----- //
+    public static final int Id_Game_Activity = 42;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         textView = (TextView) findViewById(R.id.text);
         mNameInput = (EditText) findViewById(R.id.name);
         playButton = (Button) findViewById(R.id.play);
+
         user = new User();
+
+        sharedPreferences =  getPreferences(MODE_PRIVATE);
 
         playButton.setEnabled(false);
 
@@ -52,9 +67,28 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 String firstName = mNameInput.getText().toString();
                 user.setFirstName(firstName);
+
+                sharedPreferences.edit().putString(PREF_KEY_USERNAME,user.getFirstName()).apply();
+
                 Intent gameActivityIntent = new Intent(MainActivity.this, gameActivity.class);
-                startActivity(gameActivityIntent);
+                //startActivity(gameActivityIntent);
+                startActivityForResult(gameActivityIntent,Id_Game_Activity);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(requestCode == Id_Game_Activity && RESULT_OK == resultCode) {
+            int score = data.getIntExtra(gameActivity.valeur_score,0);
+            sharedPreferences.edit().putInt(PREF_KEY_SCORE,score).apply();
+
+            String userNameP =sharedPreferences.getString(PREF_KEY_USERNAME, null);
+
+            if(PREF_KEY_USERNAME != null){
+                int scoreF =sharedPreferences.getInt(PREF_KEY_SCORE, 0);
+                textView.setText("Soiyer le bienvenu " +userNameP+" votre score  est :"+scoreF);
+            }
+        }
     }
 }
--- app/src/main/java/com/example/myapplication/Controller/gameActivity.java
+++ app/src/main/java/com/example/myapplication/Controller/gameActivity.java
@@ -1,7 +1,10 @@
 package com.example.myapplication.Controller;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -27,10 +30,17 @@ public class gameActivity extends AppCompatActivity implements View.OnClickListe
     private Question currentQuestion;
     private QuestionBank questionBank;
 
+    private int score;
+    private int numberOfQuestion;
+
+    public static final String valeur_score = "valeur_score";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        numberOfQuestion = 4;
 
         question = findViewById(R.id.question);
         answer1 = findViewById(R.id.answer1);
@@ -40,15 +50,26 @@ public class gameActivity extends AppCompatActivity implements View.OnClickListe
 
         questionBank = this.generateQuestion();
 
-        answer1.setOnClickListener(this);
+       /* answer1.setOnClickListener(this);
         answer2.setOnClickListener(this);
         answer3.setOnClickListener(this);
         answer4.setOnClickListener(this);
+
+                ==
+
+        for(View button: new View[]{answer1,answer2,answer3,answer4}){
+            button.setOnClickListener(gameActivity.this);
+        }
+        */
 
         answer1.setTag(0);
         answer2.setTag(1);
         answer3.setTag(2);
         answer4.setTag(3);
+
+        for(View button: new View[]{answer1,answer2,answer3,answer4}){
+            button.setOnClickListener(gameActivity.this);
+        }
 
         currentQuestion = questionBank.getQuestion();
         this.displayQuestion(currentQuestion);
@@ -72,7 +93,31 @@ public class gameActivity extends AppCompatActivity implements View.OnClickListe
         Question question3 = new Question("What is the house number of The Simpsons?",
                 Arrays.asList("42", "101", "666", "742"), 3);
 
-        return new QuestionBank(Arrays.asList(question1,question2,question3));
+        Question question4 = new Question("When did the first man land on the moon?",
+                Arrays.asList("1958", "1962", "1967", "1969"),
+                3);
+
+        Question question5 = new Question("What is the capital of Romania?",
+                Arrays.asList("Bucarest", "Warsaw", "Budapest", "Berlin"),
+                0);
+
+        Question question6 = new Question("Who did the Mona Lisa paint?",
+                Arrays.asList("Michelangelo", "Leonardo Da Vinci", "Raphael", "Carravagio"),
+                1);
+
+        Question question7 = new Question("In which city is the composer Frédéric Chopin buried?",
+                Arrays.asList("Strasbourg", "Warsaw", "Paris", "Moscow"),
+                2);
+
+        Question question8 = new Question("What is the country top-level domain of Belgium?",
+                Arrays.asList(".bg", ".bm", ".bl", ".be"),
+                3);
+
+        Question question9 = new Question("What is the house number of The Simpsons?",
+                Arrays.asList("42", "101", "666", "742"),
+                3);
+
+        return new QuestionBank(Arrays.asList(question1,question2,question3,question4,question5,question6,question7,question8,question9));
     }
 
     private void displayQuestion(final Question qst){
@@ -82,15 +127,40 @@ public class gameActivity extends AppCompatActivity implements View.OnClickListe
         answer3.setText(qst.getChoiceList().get(2));
         answer4.setText(qst.getChoiceList().get(3));
     }
+    //region onClick implementation
     @Override
     public void onClick(View view) {
         int userAnswer = (int) view.getTag();
         if(userAnswer == currentQuestion.getAnswerIndex()){
             // ---- Correct answer ---- //
             Toast.makeText(gameActivity.this,"Goog job",Toast.LENGTH_LONG).show();
+            score++;
         }else{
             // ---- Incorrect answer ---- //
             Toast.makeText(gameActivity.this, "Uh oh, that's wrong! :(",Toast.LENGTH_LONG).show();
         }
+        if(--numberOfQuestion == 0){
+            // --- end Game --- //
+            endGame();
+        }else{
+            currentQuestion = questionBank.getQuestion();
+            displayQuestion(currentQuestion);
+        }
+    }
+    //endregion
+
+    private void endGame(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setTitle("Well done").setMessage("Your score is "+score)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Intent intent = new Intent();
+                        intent.putExtra(valeur_score,score);
+                        setResult(RESULT_OK,intent);
+                        finish();
+                    }
+                }).create().show();
     }
 }
